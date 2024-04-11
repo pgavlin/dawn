eac93b6b5ccccf6944d7407d1f62a22c07d2d922
Pat Gavlin <pgavlin@gmail.com>
2024-04-11 12:44:00 -0700

    Use caching in GHA (#4)
    This revealed an issue with the `venv` target due to a missing `generates` argument.

--- init.go
+++ init.go
@@ -3,6 +3,7 @@ package dawn
 import "go.starlark.net/resolve"
 
 func init() {
+	// Relax some Starlark restrictions.
 	resolve.AllowRecursion = true
 	resolve.AllowGlobalReassign = true
 	resolve.AllowSet = true
